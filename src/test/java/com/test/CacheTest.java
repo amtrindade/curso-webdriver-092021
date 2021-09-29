a59e1f5f20554658f8ddfdf205e0ff479fad8260
amtrindade <amtrindade@gmail.com>
2021-09-29 20:33:42 -0300

    Adiciona teste de validação da cache

---
+++ src/test/java/com/test/CacheTest.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CacheTest {
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", 
+				"/home/antonio/dev/drivers/chromedriver");				
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://antoniotrindade.com.br/treinoautomacao");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+	
+	@Test
+	public void testNavigationCache() {
+		WebElement btnCalculadora = driver.findElement(By.linkText("Calculadora"));
+		btnCalculadora.click();
+		
+		assertEquals("Desafio Automação Cálculos", driver.getTitle());
+		
+		WebElement btnTable = driver.findElement(By.linkText("Localizar Table"));
+		btnTable.click();
+		
+		assertEquals("Trabalhando com tables", driver.getTitle());
+		
+		driver.navigate().back();
+		assertEquals("Desafio Automação Cálculos", driver.getTitle());
+		
+		driver.navigate().back();
+		assertEquals("Treino Automação de Testes", driver.getTitle());
+		
+		driver.navigate().forward();
+		assertEquals("Desafio Automação Cálculos", driver.getTitle());
+		
+		driver.navigate().forward();
+		assertEquals("Trabalhando com tables", driver.getTitle());
+		
+		driver.navigate().forward();
+		assertEquals("Trabalhando com tables", driver.getTitle());
+	}
+	
+	
+}
